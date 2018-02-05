1d25354fd8a50c8c3c262c996bbf9d407f7eab31
Pinky Kohsuwan <pkohsuwan@Pinkys-MacBook-Pro.local>
2018-02-04 22:38:29 -0800

    able to full titles but layout is not working correctly

---
+++ week1-Assignment-Flix-Demo/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  week1-Assignment-Flix-Demo
+//
+//  Created by Pinky Kohsuwan on 2/4/18.
+//  Copyright © 2018 Pinky Kohsuwan. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
